902308e0a336454c548f842ee44cc75c9ad6de81
Salvador Elizarraras <salvadorbfm@gmail.com>
2013-07-11 02:57:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexFile = fs.readFileSync('index.html');
+  response.send( indexFile.toString() );
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
